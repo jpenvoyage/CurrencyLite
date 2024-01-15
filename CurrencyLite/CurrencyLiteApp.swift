6ff3373fae17218b2c0a49180a996b7e71837951
joaobug <joaoenfrance@gmail.com>
2024-01-15 18:24:03 -0300

    Initial Commit

---
+++ CurrencyLite/CurrencyLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyLiteApp.swift
+//  CurrencyLite
+//
+//  Created by Joao Leal on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
