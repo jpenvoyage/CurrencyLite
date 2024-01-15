2db1ed98b7b4c2a5c5612580f8286d106780b570
joaobug <joaoenfrance@gmail.com>
2024-01-15 18:37:43 -0300

    Start of the App

--- CurrencyLite/ContentView.swift
+++ CurrencyLite/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var itemSelected = 0
+    @State private var itemSelected2 = 1
+    @State private var amount: String = ""
+     private let currencies = ["USD", "GBP", "BRL"]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section(header: Text("Convert a currency")) {
+                    TextField("Enter an amount", text: $amount)
+                        .keyboardType(.decimalPad)
+                    
+                        Picker(selection: $itemSelected, label: Text("From")) {
+                            ForEach(0 ..< currencies.count, id: \.self) { index in Text(self.currencies[index]).tag(index)}
+                        }
+                }
+            }
         }
-        .padding()
     }
 }
 
